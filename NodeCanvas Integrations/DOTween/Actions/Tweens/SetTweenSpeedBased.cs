49b8b1ec90bc7d40ec28c784093ebf4982e9d930
Alexander Ren <alex.ren@gmail.com>
2015-12-06 10:42:43 -0800

    Converted tabs to spaces.

--- NodeCanvas Integrations/DOTween/Actions/Tweens/SetTweenSpeedBased.cs
+++ NodeCanvas Integrations/DOTween/Actions/Tweens/SetTweenSpeedBased.cs
@@ -6,37 +6,35 @@ using ParadoxNotion.Design;
 
 namespace NodeCanvas.Tasks.DOTween.Tweens
 {
-	[Category("DOTween/Tweens")]
-	[Name("Set Tween Speed Based")]
-	[Description("If IsSpeedBased is True, sets the tween as speed based " +
-		"(the duration will represent the number of units/degrees the tween moves x second). " +
-		"\n\nNOTE: if you want your speed to be constant, also set the ease to Ease.Linear. " +
-		"\n\nHas no effect if the tween has already started or is inside a Sequence.")]
-	[Icon("DOTweenTween")]
-	public class SetTweenSpeedBased : ActionTask
-	{
-		[RequiredField]
-		[BlackboardOnly]
-		public BBParameter<Tween> Tween;
+    [Category ("DOTween/Tweens")]
+    [Name ("Set Tween Speed Based")]
+    [Description ("If IsSpeedBased is True, sets the tween as speed based " +
+    "(the duration will represent the number of units/degrees the tween moves x second). " +
+    "\n\nNOTE: if you want your speed to be constant, also set the ease to Ease.Linear. " +
+    "\n\nHas no effect if the tween has already started or is inside a Sequence.")]
+    [Icon ("DOTweenTween")]
+    public class SetTweenSpeedBased : ActionTask
+    {
+        [RequiredField]
+        [BlackboardOnly]
+        public BBParameter<Tween> Tween;
 
-		[RequiredField]
-		public BBParameter<bool> IsSpeedBased;
+        [RequiredField]
+        public BBParameter<bool> IsSpeedBased;
 
-		protected override string info
-		{
-			get
-			{
-				var descriptionBuilder = new StringBuilder();
-				descriptionBuilder.AppendFormat("Set Speed Based {0}", IsSpeedBased);
+        protected override string info {
+            get {
+                var descriptionBuilder = new StringBuilder ();
+                descriptionBuilder.AppendFormat ("Set Speed Based {0}", IsSpeedBased);
 
-				return descriptionBuilder.ToString();
-			}
-		}
+                return descriptionBuilder.ToString ();
+            }
+        }
 
-		protected override void OnExecute()
-		{
-			Tween.value.SetSpeedBased(IsSpeedBased.value);
-			EndAction(true);
-		}
-	}
+        protected override void OnExecute ()
+        {
+            Tween.value.SetSpeedBased (IsSpeedBased.value);
+            EndAction (true);
+        }
+    }
 }
